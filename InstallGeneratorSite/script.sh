aa6f616bbc646d59e4dcbc691bd7d306a9c8e9a0
albiGR <albi.grainca@uha.fr>
2023-04-13 16:56:40 +0200

    modification du script d installation du generateur

--- InstallGeneratorSite/script.sh
+++ InstallGeneratorSite/script.sh
@@ -24,10 +24,76 @@ npm i -D pug
 cd ../
 
 cp ./script.sh "$directory_name"/
-cp ./generateSite.pug "$directory_name"/
-cp ./template.css "$directory_name"/
-cp ./template.pug "$directory_name"/
+
+# Création des fichiers pour le site originel
+
+echo "body {
+	background: rgb(255, 255, 255);
+	color: rgb(19, 1, 1);
+	font-size: larger; 
+}
+
+.container {
+	max-width: 800px;
+}
+
+#title {
+	height: 85px;
+	margin-top: 20px;
+	margin-bottom: 20px;
+	padding-top: 20px;
+	border: 1px solid;
+}
+
+#footer {
+	position: fixed;
+	width: 100%;
+	bottom: 10px;
+	left: 0;
+	text-align: center;
+}
+
+a, .btn-link, a:hover, .btn-link:hover {
+	text-decoration: none;
+	color: rgb(3, 1, 1);
+	font-style: italic;
+}
+" > "$directory_name/template.css"
+
+echo "html
+	head
+		meta(charset="utf-8")
+		meta(name="viewport",content="width=device-width,initial-scale=1")
+		title Test
+		link(href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css",rel="stylesheet")
+		link(href="/template.css",rel="stylesheet")
+	body
+		.container
+			.row
+				.col.text-center
+					h1#title Le meilleur site
+			.row
+				.col
+					block content
+			div#footer Fait par albi
+" > "$directory_name/template.pug"
+
+echo "extends template
+block content
+	form(action="/generateSite",method="POST")
+		.form-group
+			label Nom du répertoire :
+			input(name="nom").form-control
+		.form-group
+			label Port
+			input(name="port").form-control
+		button.btn.btn-link Create
+" > "$directory_name/generateSite.pug"
+
+
 chmod -R a+rwx "$directory_name"/script.sh
+chmod -R a+rwx "$directory_name"/template.css
+chmod -R a+rwx "$directory_name"/template.pug
 chmod -R a+rwx "$directory_name"/generateSite.pug
 
 
