327b8282c6452c8869274c182ee03831949c4afa
Batuhan Goker <habbgoker@outlook.fr>
2023-04-17 14:14:53 +0200

    ajout des pages create, edit et list

--- website_deployment/server.js
+++ website_deployment/server.js
@@ -3,14 +3,26 @@ const app = express();
 const { exec } = require('child_process');
 const root = __dirname;
 app.use(express.static(root));
-const port = 3030;
+const port = 3080;
 
 app.use(express.urlencoded({ extended: true })); // req.body
 app.set('view engine','pug');
 app.set('views',root);
 
-app.get('/generate', (req, res) => {
-  res.render('generateSite');
+app.get('/index', (req, res) => {
+	res.render('index');
+  });
+
+  app.get('/create', (req, res) => {
+	res.render('create');
+  });
+
+  app.get('/edit', (req, res) => {
+	res.render('edit');
+  });
+
+app.get('/list', (req, res) => {
+  res.render('list');
 });
 
 app.post('/generateSite',(req,res)=>{
@@ -21,7 +33,7 @@ app.post('/generateSite',(req,res)=>{
 });
 
 app.get('/', (req, res) => {
-	res.redirect('/generate');
+	res.redirect('/index');
 });
 
 app.listen(port, () => {
