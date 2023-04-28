179bc8f1b2e7969011974bafd11cd7a85042f3f8
Batuhan Goker <habbgoker@outlook.fr>
2023-04-28 16:07:47 +0200

    suppresion du site

--- website_deployment/my_site/config.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Use the sed command to replace placeholders in the template file
-sed -e "s#<LOGO>#$LOGO#g" -e "s#<HEADER>#$HEADER#g" -e "s#<FOOTER>#$FOOTER#g" template > template.pug
