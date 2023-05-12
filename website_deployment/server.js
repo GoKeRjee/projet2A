2ae9f348a9748f511d8d07db9531581a8321c5c8
albiGR <albi.grainca@uha.fr>
2023-05-12 19:36:45 +0200

    ajout de l option de login

--- website_deployment/server.js
+++ website_deployment/server.js
@@ -4,12 +4,17 @@ const net = require('net');
 const { exec } = require('child_process');
 const bcrypt = require('bcryptjs');
 const { body, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+
 
 const root = __dirname;
 app.use(express.static(root));
 const port = 3030;
 
 app.use(express.urlencoded({ extended: true })); // req.body
+app.use(cookieParser());
 app.set('view engine','pug');
 app.set('views',root);
 
@@ -17,56 +22,84 @@ const db = require('monk')('127.0.0.1:27017/mySites');
 const sitesCollection = db.get('sites');
 const usersCollection = db.get('users');
 
-app.get('/index', (req, res) => {
+// Auth middleware
+const isAuth = (req, res, next) => {
+	if (req.cookies && req.cookies.token) {
+		const token = req.cookies.token;
+		if (!token) {
+			const error = new Error('Not authenticated. No token found.');
+			error.statusCode = 401;
+			throw error;
+		}
+		let decodedToken;
+		try {
+			decodedToken = jwt.verify(token, `${process.env.SECRET_KEY}`);
+		} catch (err) {
+			err.statusCode = 500;
+			throw err;
+		}
+		if (!decodedToken) {
+			const error = new Error('Not authenticated. Invalid token.');
+			error.statusCode = 401;
+			throw error;
+		}
+		req.userId = decodedToken.userId;
+		next();
+	} else {
+		res.redirect('/login');
+	}
+};
+
+// Pages
+app.get('/index', isAuth, (req, res) => {
 	res.render('index');
 });
 
-app.get('/isPortUsed/:port', async (req, res) => {
+app.get('/isPortUsed/:port', isAuth, async (req, res) => {
 	const port = req.params.port;
 	const portExists = await sitesCollection.findOne({ port });
-  
 	res.json({ portUsed: !!portExists });
 });
 
-app.get('/isSiteNameUsed/:name', async (req, res) => {
-try {
-	const name = req.params.name;
-	const site = await sitesCollection.findOne({ name });
-	res.json({ nameUsed: !!site });
-} catch (err) {
-	console.error('Error checking site name availability:', err);
-	res.status(500).json({ nameUsed: false });
-}
+app.get('/isSiteNameUsed/:name',isAuth, async (req, res) => {
+	try {
+		const name = req.params.name;
+		const site = await sitesCollection.findOne({ name });
+		res.json({ nameUsed: !!site });
+	} catch (err) {
+		console.error('Error checking site name availability:', err);
+		res.status(500).json({ nameUsed: false });
+	}
 });
 
-app.get('/isDbNameUsed/:dbname', async (req, res) => {
+app.get('/isDbNameUsed/:dbname', isAuth, async (req, res) => {
 	const dbname = req.params.dbname;
 	const dbnameExists = await sitesCollection.findOne({ dbname });
 	res.json({ dbnameUsed: !!dbnameExists });
 });  
 
-app.get('/create', (req, res) => {
+app.get('/create', isAuth, (req, res) => {
 	res.render('create');
 });
 
-app.get('/edit', async (req, res) => {
+app.get('/edit', isAuth, async (req, res) => {
 	try {
-	  const sites = await sitesCollection.find({});
-	  res.render('edit', { sites, noSites: sites.length === 0 });
+	  	const sites = await sitesCollection.find({});
+	  	res.render('edit', { sites, noSites: sites.length === 0 });
 	} catch (err) {
-	  throw err;
+	  	throw err;
 	}
 });  
 
-app.get('/list', async (req, res) => {
+app.get('/list', isAuth, async (req, res) => {
 	try {
-	  const sites = await sitesCollection.find({});
-	  for (let site of sites) {
-		site.status = (await isPortUsed(site.port)) ? 'ON' : 'OFF';
-	  }
-	  res.render('list', { sites });
+		const sites = await sitesCollection.find({});
+	  	for (let site of sites) {
+			site.status = (await isPortUsed(site.port)) ? 'ON' : 'OFF';
+	  	}
+	  	res.render('list', { sites });
 	} catch (err) {
-	  throw err;
+	  	throw err;
 	}
 });
 
@@ -74,11 +107,41 @@ app.get('/login', (req, res) => {
 	res.render('login');
 });
 
-app.get('/registration', (req, res) => {
+app.post('/login', async(req, res) => {
+	const email = req.body.email;
+    const password = req.body.password;
+	let loadedUser;
+	try {
+        const user = await usersCollection.findOne({ email: email});
+        if (!user) {
+            return res.render('login', { error: 'A user with this email could not be found.' });
+        }
+        const isEqual = await bcrypt.compare(password, user.password);
+        if(!isEqual) {
+            return res.render('login', { error: 'Invalid password.' });
+        }
+        loadedUser = user;
+        const token = jwt.sign({
+                email: loadedUser.email,
+                userId: loadedUser._id.toString()
+            },
+            `${process.env.SECRET_KEY}`,
+            { expiresIn: '1h' }
+        );
+        res.cookie('token', token, { httpOnly: true, sameSite: 'strict' });
+        res.redirect('/index');
+    } catch (err) {
+        if(!err.statusCode) err.statusCode = 500;
+        return res.render('login', { error: 'An unexpected error occurred. Please try again.' });
+    }
+});
+
+
+app.get('/signup', (req, res) => {
 	res.render('registration');
 });	
 
-app.post('/register',
+app.post('/signup',
   [
     body('email').isEmail()
       .withMessage('Please enter a valid email.')
@@ -94,7 +157,6 @@ app.post('/register',
 		if (value !== req.body.password) {
 		  throw new Error('Password confirmation does not match password');
 		}
-		// Indicates the success of this synchronous custom validator
 		return true;
 	  })
   ],
@@ -102,37 +164,33 @@ app.post('/register',
     try {
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
-		  const errorData = errors.array().map(error => {
-			return { type: 'field', value: error.value, msg: error.msg, path: error.param, location: 'body' };
-		  });
-		  // Rediriger vers la page "registration" et passer les erreurs à la vue
-		  return res.render('registration', { errors: errorData });
+			const errorData = errors.array().map(error => {
+				return { type: 'field', value: error.value, msg: error.msg, path: error.param, location: 'body' };
+		  	});
+		  	return res.render('registration', { errors: errorData });
 		}
 
-      const email = req.body.email;
-      const password = req.body.password;
-      const hashedPw = await bcrypt.hash(password, 12);
-      const newUser = {
-        email: email,
-        password: hashedPw
-      };
-      usersCollection.insert(newUser);
-      res.redirect('/login');
+      	const email = req.body.email;
+      	const password = req.body.password;
+      	const hashedPw = await bcrypt.hash(password, 12);
+      	const newUser = {
+			email: email,
+        	password: hashedPw
+      	};
+      	usersCollection.insert(newUser);
+      	res.redirect('/login');
     } catch (err) {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
+		if (!err.statusCode) err.statusCode = 500;
+	  	return res.render('registration', { errors: 'An unexpected error occurred. Please try again.' });
     }
-  }
-);
+});
 
-app.post('/createSite',(req,res)=>{
+app.post('/createSite', isAuth,(req,res)=>{
 	var directory = req.body.directory;
 	var name = req.body.name;
 	var port = req.body.port;
 	var dbname = req.body.dbname;
 	exec(root + '/script.sh ' + directory + ' ' + port + ' "' + name + '" ' + dbname);
-
 	// save data on db
 	const newSite = {
 		directory: directory,
@@ -141,11 +199,10 @@ app.post('/createSite',(req,res)=>{
 		dbname: dbname
 	}
 	sitesCollection.insert(newSite);
-
 	res.redirect('/list');
 });
 
-app.post('/deleteSite', (req, res)=> {
+app.post('/deleteSite', isAuth, (req, res)=> {
 	const id = req.body.id;
 	sitesCollection.findOne({_id: id})
     .then((site) => {
@@ -163,7 +220,7 @@ app.post('/deleteSite', (req, res)=> {
     });
 });
 
-app.post('/stopSite', (req, res)=> {
+app.post('/stopSite', isAuth, (req, res)=> {
 	const id = req.body.id;
 	sitesCollection.findOne({_id: id})
     .then((site) => {
@@ -178,7 +235,7 @@ app.post('/stopSite', (req, res)=> {
     });
 });
 
-app.post('/startSite', (req, res)=> {
+app.post('/startSite', isAuth, (req, res)=> {
 	const id = req.body.id;
 	sitesCollection.findOne({_id: id})
     .then((site) => {
@@ -196,73 +253,64 @@ app.post('/startSite', (req, res)=> {
 
 const isPortUsed = (port) => {
 	return new Promise((resolve) => {
-	  const server = net.createServer();
-	  server.once('error', (err) => {
-		if (err.code === 'EADDRINUSE' || err.code === 'EACCES') {
-		  resolve(true);
-		} else {
-		  resolve(false);
-		}
-	  });
-
-	  server.once('listening', () => {
-		server.close();
-		resolve(false);
-	  });
-
-	  server.listen(port, '127.0.0.1');
+		const server = net.createServer();
+		server.once('error', (err) => {
+			if (err.code === 'EADDRINUSE' || err.code === 'EACCES') {
+				resolve(true);
+			} else {
+				resolve(false);
+			}
+		});
+		server.once('listening', () => {
+			server.close();
+			resolve(false);
+		});
+		server.listen(port, '127.0.0.1');
 	});
 };
 
-app.post('/updateSite', async (req, res) => {
-  const id = req.body.id;
-  const newName = req.body.name;
-  const newPort = req.body.port;
+app.post('/updateSite', isAuth, async (req, res) => {
+	const id = req.body.id;
+	const newName = req.body.name;
+	const newPort = req.body.port;
 
-  try {
-    const site = await sitesCollection.findOne({ _id: id });
-    if (site) {
-      const oldPort = site.port;
-      const directory = site.directory;
-
-      // Update site in the database
-      await sitesCollection.update({ _id: id }, { $set: { name: newName, port: newPort } });
-
-      // Update the site configuration
-      exec(root + '/update.sh ' + directory + ' ' + oldPort + ' ' + newPort + ' "' + newName + '"');
-
-      setTimeout(() => {
-        res.redirect('/list');
-      }, 1000);
-    } else {
-      res.status(404).send('Site not found');
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server error');
-  }
+	try {
+		const site = await sitesCollection.findOne({ _id: id });
+		if (site) {
+			const oldPort = site.port;
+			const directory = site.directory;
+			// Update site in the database
+			await sitesCollection.update({ _id: id }, { $set: { name: newName, port: newPort } });
+			// Update the site configuration
+			exec(root + '/update.sh ' + directory + ' ' + oldPort + ' ' + newPort + ' "' + newName + '"');
+			setTimeout(() => { res.redirect('/list'); }, 1000);
+		} else {
+			res.status(404).send('Site not found');
+		}
+	} catch (err) {
+		console.error(err);
+		res.status(500).send('Server error');
+	}
 });
   
-app.get('/', (req, res) => {
+app.get('/', isAuth, (req, res) => {
 	res.redirect('/index');
+});
+
+app.get('/siteStatus', isAuth, async (req, res) => {
+	try {
+		const sites = await sitesCollection.find({});
+	  	const statusPromises = sites.map(async (site) => {
+			const status = await isPortUsed(site.port) ? 'ON' : 'OFF';
+			return { _id: site._id, status };
+	  	});
+	  	const statuses = await Promise.all(statusPromises);
+	  	res.json(statuses);
+	} catch (err) {
+		res.status(500).json([]);
+	}
 });
 
 app.listen(port, () => {
 	console.log(`Server listening at http://localhost:${port}`);
 });
-
-app.get('/siteStatus', async (req, res) => {
-	try {
-	  const sites = await sitesCollection.find({});
-	  const statusPromises = sites.map(async (site) => {
-		const status = await isPortUsed(site.port) ? 'ON' : 'OFF';
-		return { _id: site._id, status };
-	  });
-	  const statuses = await Promise.all(statusPromises);
-	  res.json(statuses);
-	} catch (err) {
-	  console.error('Erreur lors de la récupération des statuts des sites:', err);
-	  res.status(500).json([]);
-	}
-  });
-  
