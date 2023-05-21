ab31017d5c8c0143107980ef6ff16c869158c5c9
albiGR <albi.grainca@uha.fr>
2023-05-21 16:12:48 +0200

    ajout de la création de l utilisateur admin par defaut

--- installGeneratorSite/installer.sh
+++ installGeneratorSite/installer.sh
@@ -2346,12 +2346,56 @@ app.listen(port, () => {
 });
 EOF
 
+cat << 'EOF' > "$directory_name/createAdmin.js"
+const bcrypt = require('bcryptjs');
+const db = require('monk')('127.0.0.1:27017/mySites');
+const usersCollection = db.get('users');
+
+const createAdminAccount = async () => {
+    try {
+        // Check if the admin account already exists
+        const adminDoc = await usersCollection.findOne({ status: 'admin' });
+        if (adminDoc) {
+            console.log('The administrator account already exists.');
+            return;
+        }
+        // Ask for the login information for the administrator account
+        const adminEmail = 'admin@example.com';
+        const adminPassword = 'adminPassword';
+        // Hash the password
+        const hashedAdminPw = await bcrypt.hash(adminPassword, 12);
+        // Create the admin account
+        const adminUser = {
+            email: adminEmail,
+            password: hashedAdminPw,
+            status: 'admin',
+            approved: true, // Assuming the admin account is approved by default
+            tokenVersion: 0,
+        };  
+        // Insert the admin account into the users collection
+        await usersCollection.insert(adminUser);
+        console.log('The administrator account has been successfully created.');
+    } catch (err) {
+        console.error('An error occurred while creating the administrator account:', err);
+    } finally {
+        // Close the database connection
+        db.close();
+    }
+}
+
+// Call the function to create the administrator account
+createAdminAccount();
+EOF
+
 # Move the folder img to "TheGenerator" folder
 chmod -R a+rwx img
 mv img $directory_name
 
 # Confirm the creation of the folders
 echo "Site generated in the folder $directory_name."
+
+# Create admin user if is not create
+node "$directory_name"/createAdmin.js
 
 # Launch the service
 node "$directory_name"/server.js &
